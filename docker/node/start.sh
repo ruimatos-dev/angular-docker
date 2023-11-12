bb5079621b1d2647c20d7feb5da5a443c84d379d
Rui Matos <rui.microk@gmail.com>
2023-11-12 20:06:10 -0300

    UPDATED /app/start.sh container

--- docker/node/start.sh
+++ docker/node/start.sh
@@ -3,8 +3,17 @@
 ## Transformar a pasta for user node and group node
 chown -R node:node blog/
 
-## Copy all file preservando os atributos. -p é responsável por isso.
-cp -R -p blog/  /appaux/
+# /app/blog é nativo da imagem
+# preciso verificar se /appaux/blog tem
+# se não tiver copia o projeto da imagem para volume compartilhado
+if [ -d /appaux/blog];
+then
+  echo "The directory Exists"
+else
+  echo "The directory is not present"
+  cp -R -p blog/  /appaux/  ## Copy all file preservando os atributos. -p é responsável por isso.
+  echo "Directory blog create.It is paste new project"
+fi
 
 cd /appaux/blog
 
